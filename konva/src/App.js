7b81a43bd4f33053facf4fbbd4428c6ffaf86e5d
heyungao <heyg@cunxin.com>
2021-07-02 18:38:39 +0800

    update

---
+++ konva/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Stage, Layer, Text } from 'react-konva';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        isDragging: false,
+        x: 50,
+        y: 50
+    };
+  }
+
+  render() {
+
+    return (
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <Text
+                text="Draggable Text"
+                x={this.state.x}
+                y={this.state.y}
+                draggable
+                fill={this.state.isDragging ? 'green' : 'black'}
+                onDragStart={() => {
+                  this.setState({
+                    isDragging: true
+                  });
+                }}
+                onDragEnd={e => {
+                  this.setState({
+                    isDragging: false,
+                    x: e.target.x(),
+                    y: e.target.y()
+                  });
+                }}
+            />
+          </Layer>
+        </Stage>
+    );
+  }
+}
+
+export default App;
