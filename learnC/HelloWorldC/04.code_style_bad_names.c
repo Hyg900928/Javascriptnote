8030cbc91d937b95636b5e374d80b61ab922ca81
heyungao <heyg@cunxin.com>
2023-02-07 23:07:10 +0800

    update

---
+++ learnC/HelloWorldC/04.code_style_bad_names.c
@@ -0,0 +1,15 @@
+//
+// Created by heyungao on 2023/2/7.
+//
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int i = 0;
+    while (i < 10) {
+        a = a + i;
+        i++;
+    }
+    printf("a=%d\n", a);
+    return 0;
+}
---
+++ learnC/HelloWorldC/05.code_style_good_names.c
@@ -0,0 +1,24 @@
+//
+// Created by heyungao on 2023/2/7.
+
+#include <stdio.h>
+
+
+/**
+ * this program calculator summary from 0 to 9
+ * @param argc parameter count
+ * @param argv parameters
+ * @return 0
+ */
+int main(int argc, char **argv ) {
+
+    // 变量命名要明确 比如用sum 变量
+    int sum = 0;
+    int i = 0;
+    while (i < 10) {
+        sum = sum + i;
+        i++;
+    }
+    printf("a = %d\n", sum);
+    return 0;
+}
